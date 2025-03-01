c65a9f146e7c32cdd92ebf46664e6423efec776e
Marcos Tullio Silva de Souza <marcos.souza@hi-mix.com.br>
2025-03-01 18:04:40 -0300

    finalizado o desafio do tabuleiro

---
+++ src/components/Tabuleiro.jsx
@@ -0,0 +1,14 @@
+import Linha from "@/components/Linha";
+
+export default function Tabuleiro() {
+  return <div>
+    <Linha />
+    <Linha preto/>
+    <Linha />
+    <Linha preto/>
+    <Linha />
+    <Linha preto/>
+    <Linha />
+    <Linha preto/>
+  </div>
+}
